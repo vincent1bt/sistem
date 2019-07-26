3bf0e33a08f7436415c10a0f92ab9739b3977f34
vincent1bt <vincent14319@gmail.com>
2019-07-26 18:31:48 -0500

    app started

---
+++ lorfsistem/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+# class Item(models.Model):
+#     name = models.TextField()
+#     code = models.TextField()
+#     stock = models.IntegerField()
+#     price = models.FloatField()
+
+# class Transaction(models.Model):
+#     name = models.TextField()
+#     code = models.TextField()
+#     variant_price = models.FloatField()
+#     sale = models.BooleanField(default=True)
+#     date = models.DateField(default=date.today)
+
+#     json_field
